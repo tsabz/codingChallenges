18b6c5d232e860d57ceab520edfcf16a6baf0be2
Tonia Saba <teesabz@Tonias-MacBook-Pro.local>
2020-08-03 10:53:38 -0400

    coding challenge readme started

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # codingChallengez
+
+Coding challenge can be a very intimidating part interviews, but they shouldn't be!! 
+I've put together a document of all my favorite ones, either because they were fun or 
+I just thought they were really helpful. 
+
---
+++ code.js
@@ -0,0 +1,21 @@
+console.log('coding!');
+
+
+// let array = [1,2,3]
+
+// console.log(array);
+
+
+
+// const reverse = (x) => {  return x.toString().split("").reverse().join("") 
+// };
+
+// console.log(reverse(-123));
+
+
+// const reverse = (x) => {  return x.toString().split("").reverse().join("")                 
+// };
+
+const reverse = (x) => { return x.toString().split("").reverse().join("")                    
+    //if number is 
+};
