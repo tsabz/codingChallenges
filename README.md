00c1424001e8f56fbe05315c2b5f06f837080768
Tonia Saba <36241267+tsabz@users.noreply.github.com>
2020-08-10 09:17:16 -0400

    stack push pop notes

--- README.md
+++ README.md
@@ -7,20 +7,49 @@ I've put together a document of all my favorite challenges from either LeetCode 
 
 ### what is the difference between a data structure and algorithms ?? 
 Data structures are quite literally how we structure the data we are working with. 
+algorithms is a lot like a recipe, called a procedure. A procedure you tell your computer 
+to follow in order to solve a problem or reach a desired outcome in whatever you are creating. 
 
+#### Stack Data Structure example 
+
+```
+const stack = [];
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    console.log(stack)
+    stack.push(4)
+    // push adds 4 to the end of the array 
+    console.log(stack) 
+    //new stack array = [1, 2, 3, 4]
+    stack.pop()
+    // pop deletes 4 from the end of the array 
+    console.log(stack) 
+    //new stack array = [1, 2, 3]
+
+    //shift deletes the first number in the array
+    stack.shift()
+    //new stack array = [2, 3]
+    stack.shift()
+    //new stack array = [3]
+    console.log(stack) 
+
+    //unshift adds the first number to the array
+    stack.unshift(1, 2)
+    //new stack array = [1, 2, 3]
+    console.log(stack)
+```
 
 ### Palindrome Number
 Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
 
 ```
 const isPalindrome = (x) => {
-    //num to string
     let xnum = x.toString()
     let j = xnum.length - 1
-    //for loop for each char
+    
     for(let i = 0; i < xnum.length; i++){
-        //for loop backwards for each char
-            //if the chars is not the same return false
             if(xnum[i] !== xnum[j]) {
                 return false
         }
--- code.js
+++ code.js
@@ -66,14 +66,120 @@ console.log('coding!');
 // console.log(simpleArraySum(1,2));
 
 
-let myArray = ['a', 'b', 'c', 'd']
+// let myArray = ['a', 'b', 'c', 'd']
 
-myArray.push('end')// adds to the end of the array
-myArray.unshift('start') // adds to the beginning of the array
-myArray.pop()// deletes the last item in the array
+// myArray.push('end')// adds to the end of the array
+// myArray.unshift('start') // adds to the beginning of the array
+// myArray.pop()// deletes the last item in the array
 
 
 ///ES6 SYNTAX
-myArray = ['start',...myArray, 'end'] // es6 adds to the beginning and end 
+// myArray = ['start',...myArray, 'end'] // es6 adds to the beginning and end 
 
-console.log(myArray)
+// console.log(myArray)
+
+// ** STACK ** //
+
+// const stack = [];
+//     stack.push(1);
+//     stack.push(2);
+//     stack.push(3);
+
+//     console.log(stack)
+//     stack.push(4)
+//     // push adds 4 to the end of the array 
+//     console.log(stack) 
+//     //new stack array = [1, 2, 3, 4]
+//     stack.pop()
+//     // pop deletes 4 from the end of the array 
+//     console.log(stack) 
+//     //new stack array = [1, 2, 3]
+
+//     //shift deletes the first number in the array
+//     stack.shift()
+//     //new stack array = [2, 3]
+//     stack.shift()
+//     //new stack array = [3]
+//     console.log(stack) 
+
+//     //unshift adds the first number to the array
+//     stack.unshift(1, 2)
+//     //new stack array = [1, 2, 3]
+//     console.log(stack)
+
+
+// ** Reverse String using stack ** //
+
+// const reverseStr = (str) => {
+//     let stack = []
+//     //push each string string into stack
+
+//     for(let i = 0; i < str.length; i++) {
+//         stack.push(str[i])
+//     }
+//     let reverse = '';
+
+//     while(stack.length > 0) {
+//         reverse += stack.pop();
+//     }
+//     return reverse //Tonia Saba
+// }
+
+
+
+// const reverseStr = (str) => {
+//     let stack = []
+//     //push each string string into stack
+
+//     for(let i = 0; i < str.length; i++) {
+//         stack.push(str[i])
+//     }
+//     console.log(stack) //["T", "o", "n", "i", "a", " ", "S", "a", "b", "a"]
+//     let reverse = '';
+//     console.log(reverse) //''
+
+//     while(stack.length > 0) {
+//         console.log(stack)
+//         reverse += stack.pop();
+//         console.log(stack)
+//         console.log(reverse)
+//     }
+//     return reverse //Tonia Saba
+// }
+
+// console.log(reverseStr('Tonia Saba'))
+
+
+// console.log(reverseStr('Tonia Saba'))
+
+
+// ** while loop practice ** //
+
+const whileLoop = (str) => {
+    stack = []
+
+    for(let i = 0; i < str.length; i++){
+        stack.push(str[i])
+    }
+    let reverseStr = ''
+
+    while(stack.length > 0){
+        reverseStr += stack.pop()
+    }
+    return reverseStr
+}
+console.log(whileLoop('love'))
+// console.log(whileLoop('Tonia'))
+
+
+// ** Hash tables ** //
+
+// const contents = []
+
+// const hash = (value) => {
+//     let result = 0;
+//     for(let i = 0; i < value.length; i++){
+//         result += value.charCodeAt(i);
+//     }
+//     return result;
+// }
